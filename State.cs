9f436ce9026e442d025b3538aa5626574f3461d6
HJRayas <74790450+HJRayas@users.noreply.github.com>
2021-01-10 22:51:44 -0700

    Add files via upload

---
+++ State.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFletesAcarreoB.GOF
+{
+    class MainApp
+
+    {
+        /// <summary>
+
+        /// Entry point into console application.
+
+        /// </summary
+    }
+
+    /// <summary>
+
+    /// The 'State' abstract class
+
+    /// </summary>
+
+    abstract class State
+
+    {
+        public abstract void Handle(Context context);
+    }
+
+    /// <summary>
+
+    /// A 'ConcreteState' class
+
+    /// </summary>
+
+    class Disponible: State
+
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new No_Disponible();
+        }
+    }
+
+    /// <summary>
+
+    /// A 'ConcreteState' class
+
+    /// </summary>
+
+    class No_Disponible : State
+
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new Disponible();
+        }
+    }
+
+    /// <summary>
+
+    /// The 'Context' class
+
+    /// </summary>
+
+    class Context
+
+    {
+        private State _state;
+
+        // Constructor
+
+        public Context(State state)
+        {
+            this.State = state;
+        }
+
+        // Gets or sets the state
+
+        public State State
+        {
+            get { return _state; }
+            set
+
+            {
+                _state = value;
+                Console.WriteLine("Estado: " +
+                  _state.GetType().Name);
+            }
+        }
+
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+    }
+}
